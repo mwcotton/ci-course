79133cad685ab382bc1816c444db35387b5a2bc1
mwcotton <22508607+mwcotton@users.noreply.github.com>
2020-10-19 15:42:42 +0100

    Testing line 36

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -14,6 +14,9 @@ class TestFunctionality(unittest.TestCase):
         self.assertEqual(ci_course.minimum(1.2, 2.3), 1.2)
         self.assertEqual(ci_course.minimum(-1.2, -3), -3)
 
+    def test_minimum_empty(self):
+        self.assertEqual(ci_course.minimum(), None)
+
 
 if __name__ == '__main__':
     unittest.main()
